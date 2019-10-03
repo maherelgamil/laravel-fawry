fdc3cb4c678e9fbdf3626cca37d63ff13f95ab0e
maher elgamil <maherbusnes@gmail.com>
2019-10-03 15:39:24 +0200

    fix issue in migration

--- config/fawry.php
+++ config/fawry.php
@@ -2,13 +2,27 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Debug option
+    |--------------------------------------------------------------------------
+    | Accept boolean value , and toggle between the production endpoint and sandbox
+    */
+
     'debug' => env('FAWRY_DEBUG', true),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Fawry Keys
+    |--------------------------------------------------------------------------
+    |
+    | The Fawry publishable key and secret key give you access to Fawry's
+    | API.
+    */
 
     'merchant_code' => env('FAWRY_MERCHANT_CODE'),
 
     'security_key' => env('FAWRY_SECURITY_KEY'),
-
-    'path' => 'fawry',
 
     'user_table' => 'users',
 
--- database/migrations/2019_09_29_154000_create_payment_cards_table.php
+++ database/migrations/2019_09_29_154000_create_payment_cards_table.php
@@ -13,7 +13,7 @@ class CreatePaymentCardsTable extends Migration
      */
     public function up()
     {
-        Schema::create(config('fawry.user_table'), function (Blueprint $table) {
+        Schema::table(config('fawry.user_table'), function (Blueprint $table) {
             $table->string('payment_card_last_four')->unique();
             $table->string('payment_card_brand');
             $table->text('payment_card_fawry_token')->nullable();
@@ -27,7 +27,7 @@ class CreatePaymentCardsTable extends Migration
      */
     public function down()
     {
-        Schema::create(config('fawry.user_table'), function (Blueprint $table) {
+        Schema::table(config('fawry.user_table'), function (Blueprint $table) {
             $table->dropColumn('payment_card_last_four')->unique();
             $table->dropColumn('payment_card_brand');
             $table->dropColumn('payment_card_fawry_token')->nullable();
